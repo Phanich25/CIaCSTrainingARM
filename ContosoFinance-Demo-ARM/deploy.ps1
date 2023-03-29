131b86e1ed5e0f79458396ef86752b8039b2819e
Phanich25 <76997439+Phanich25@users.noreply.github.com>
2023-03-29 10:56:38 +0530

    Update deploy.ps1

--- ContosoFinance-Demo-ARM/deploy.ps1
+++ ContosoFinance-Demo-ARM/deploy.ps1
@@ -5,7 +5,7 @@
 
 
 
-$subscriptionId = "85cd2292-82e3-4c72-a2d7-1ba724a25176" #insert your subscription ID
+$subscriptionId = "d9174032-d76b-41c6-9763-3bbe9f3cd653" #insert your subscription ID
 $resourceGroupName = "CIaCS-rg"  #provide resource group name
 $resourceGroupLocation = "WestEurope"  #location
 
